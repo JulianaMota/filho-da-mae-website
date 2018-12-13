ca992cf89d2b4544c2c73b8052468873a648754d
Juliana Mota <38047358+JulianaMota@users.noreply.github.com>
2018-12-13 19:40:14 +0100

    destai js done

--- js/details.js
+++ js/details.js
@@ -1,5 +1,5 @@
 const albumID = param.get("albumid");
-const template = document.querySelector("#album-detail-template").content;
+const template = document.querySelector("#albumDetailTemplate").content;
 console.log(albumID);
 
 function loadOneAlbum(albumID){
@@ -17,6 +17,17 @@ function showOneAlbum(data){
     }
 
     copy.querySelector(".albumTitle").textContent=data.title.rendered;
+    copy.querySelector(".albumAuthorName").textContent=data.acf.by;
+    copy.querySelector(".albumYear").textContent=data.acf.date;
+    copy.querySelector(".albumSongsNumber").textContent=data.acf.songs_number;
+    copy.querySelector(".buyLink1").href=data.acf.buy_link;
+    copy.querySelector(".buyLink2").href=data.acf.buy_link;
+    copy.querySelector(".buyLink3").href=data.acf.buy_link;
+    copy.querySelector(".albumDigitalPrice").textContent=data.acf.digital_price;
+    copy.querySelector(".albumCDPrice").textContent=data.acf.disc_price;
+    copy.querySelector(".albumVinylPrice").textContent=data.acf.vinyl_price;
+    copy.querySelector(".spotifySongs").innerHTML=data.acf.spotify_album;
+
 
     document.querySelector(".detailsPage").appendChild(copy);
 }
